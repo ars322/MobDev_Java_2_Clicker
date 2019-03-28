ae6ace543d6c7a080c016713d5c5d42c9c22886b
Arseny Smirnov <163192@edu.fa.ru>
2019-03-28 12:06:45 +0300

    laba2_Smirnov_PI3-1

--- app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
+++ app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
@@ -11,8 +11,11 @@ public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
     Button mainBtn;
+    Button mainBtn2;
+    Button mainBtn3;
 
     private long score = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +24,47 @@ public class MainActivity extends AppCompatActivity {
 
         mainText = (TextView) findViewById(R.id.mainText);
         mainBtn = (Button) findViewById(R.id.button);
-
+        mainBtn2 = (Button) findViewById(R.id.button2);
+        mainBtn3 = (Button) findViewById(R.id.button3);
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 score ++;
+                String s;
+                if (score % 10 >1 && score % 10 < 5 && (score % 100 < 10 || score % 100 > 20)) {
+                    s = "Кнопка нажата " + score + " раза";
+                }
+                else s = "Кнопка нажата " + score + " раз";
+                mainText.setText(s.toCharArray(),0, s.length());
+            }
+        };
+
+        View.OnClickListener clickListener2 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score --;
+                String s;
+                if (Math.abs(score) % 10 >1 && Math.abs(score) % 10 < 5 && (Math.abs(score) % 100 < 10 || Math.abs(score) % 100 > 20)) {
+                    s = "Кнопка нажата " + score + " раза";
+                }
+                else s = "Кнопка нажата " + score + " раз";
+                mainText.setText(s.toCharArray(),0, s.length());
+            }
+        };
+
+        View.OnClickListener clickListener3 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = 0;
                 String s = "Кликов: " + score;
                 mainText.setText(s.toCharArray(),0, s.length());
             }
         };
 
         mainBtn.setOnClickListener(clickListener);
+        mainBtn2.setOnClickListener(clickListener2);
+        mainBtn3.setOnClickListener(clickListener3);
 
     }
 }
